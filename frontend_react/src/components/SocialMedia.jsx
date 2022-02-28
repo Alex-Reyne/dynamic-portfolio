da8d93a61d46e53f3dc1addcd95b3483748d03b9
Alex-Reyne <creativereyne@gmail.com>
2022-02-27 20:03:41 -0600

    Added appropriate social media links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { SiLinktree } from 'react-icons/si';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsTwitter />
-      </div>
-      <div>
-        <BsInstagram />
-      </div>
-      <div>
-        <FaFacebookF />
-      </div>
+      <a href="https://github.com/alex-reyne" target="_blank">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
+      <a href="https://linkedin.com/in/alexanderreyne" target="_blank">
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a href="https://linktr.ee/alexreyne" target="_blank">
+        <div>
+          <SiLinktree />
+        </div>
+      </a>
     </div>
   );
 };
